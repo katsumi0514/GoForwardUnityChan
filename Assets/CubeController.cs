80cd20ad6617482310a50e73a06db3f974ac4176
伊井克海 <mt4.05140901@gamil.com>
2018-12-26 15:28:01 +0900

    レッスン７課題です。 宜しくお願いします。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+	
+	private float speed = -0.2f;
+
+	private float deadLine = -10;
+
+
+	public AudioClip impact;
+
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start(){
+
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Translate (this.speed, 0, 0);
+
+
+		if (transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+	}
+
+
+	void OnCollisionEnter2D(Collision2D other){
+		string layerName = LayerMask.LayerToName (other.gameObject.layer);
+
+		if (layerName == "Cube"  || layerName == "Ground") {
+			audioSource.PlayOneShot (impact, 0.7F);
+		}
+	
+	
+	}
+}
+
